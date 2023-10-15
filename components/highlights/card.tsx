7fb44d7b7713c6271939710a15b936282b186d74
Grbmax <grbmax@gmail.com>
2023-10-16 00:28:26 +0530

    Highlights Page

--- components/highlights/card.tsx
+++ components/highlights/card.tsx
@@ -24,9 +24,9 @@ const HighlightCard: React.FC<HighlightsData> = ({
           <ThreePlusGrid />
         </div>
       </div>
-      <div className='flex h-screen w-full items-center justify-evenly text-white'>
-        <div className='flex h-full w-full items-center justify-end'>A</div>
-        <div className='flex w-full justify-start'>B</div>
+      <div className='min-h-screen w-full flex flex-col border text-white md:flex md:flex-row md:justify-evenly'>
+        <div className='flex flex-1 md:min-h-screen border w-full items-end justify-center md:justify-end'>A</div>
+        <div className='flex flex-1 md:min-h-screen border w-full items-start justify-center md:justify-start'>B</div>
       </div>
     </div>
   );
