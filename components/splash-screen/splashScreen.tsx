6da8b4d758674a196d68e4acc5db9f2a3b582e84
Grbmax <grbmax@gmail.com>
2024-02-01 11:34:15 +0530

    Update splash screen logo and next.config.js

--- components/splash-screen/splashScreen.tsx
+++ components/splash-screen/splashScreen.tsx
@@ -14,20 +14,9 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ isWorksPage }) => {
   ) : (
     <section className='splash-screen-section absolute z-50 hidden bg-black text-white md:flex'>
       <div className='flex h-full w-full flex-col items-center justify-center gap-2'>
-        {/* <ArtistBuilding className='splash-screen-logo' />
-        <ColumnLogo className='splash-screen-logo-text' /> */}
-        <div className='splash-screen-logo relative flex h-20 w-20'>
+        <div className='splash-screen-logo relative flex h-60 w-60'>
           <Image
-            src='/website-assets/logo-building.png'
-            fill
-            className='object-fit'
-            quality={100}
-            alt='about-image'
-          />
-        </div>
-        <div className='splash-screen-logo-text relative flex h-20 w-56'>
-          <Image
-            src='/website-assets/logo-text.png'
+            src='/website-assets/splash-logo.png'
             fill
             className='object-fit'
             quality={100}
