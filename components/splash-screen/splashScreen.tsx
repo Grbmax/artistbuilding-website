942e5ba897e4aa9915ceaa03c81e67eedbe0ba46
Grbmax <grbmax@gmail.com>
2024-04-28 13:19:45 +0530

    fix bugs 28/4

--- components/splash-screen/splashScreen.tsx
+++ components/splash-screen/splashScreen.tsx
@@ -15,7 +15,7 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ isWorksPage }) => {
   ) : (
     <section className='splash-screen-section absolute z-50 hidden bg-black text-white md:flex'>
       <div className='flex h-full w-full flex-col items-center justify-center gap-2'>
-        <div className='splash-screen-logo relative flex h-14 w-14'>
+        <div className='splash-screen-logo relative flex h-14 w-14 mr-4'>
           <Image
             src='/website-assets/logo-building.png'
             fill
